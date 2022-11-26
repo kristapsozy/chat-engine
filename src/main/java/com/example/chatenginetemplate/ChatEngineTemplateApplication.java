669984a22eed2bb647f4a814efcda654d4ec4827
Kristaps Ozols <kristaps.ozols7991@gmail.com>
2022-11-26 13:11:05 +0200

    Added empty project

---
+++ src/main/java/com/example/chatenginetemplate/ChatEngineTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatenginetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatEngineTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatEngineTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatenginetemplate/ChatEngineTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatenginetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatEngineTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
